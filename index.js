737ba05a570be5c2c65f5930bce9044cc09f1c24
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-22 10:00:26 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2012", result: "L" },
+]
+
+function superbowlWin(record) {
+
+    var teamFate;
+
+    if (teamFate = record.find(item => item.result === "W")) {
+        return (teamFate.year);
+    }
+    else {
+        return (undefined);
+    }
+
+    // let result = record.find(record => record.result === "W");
+
+    // const teamFate = result ? result.year : underfined;
+    // return teamFate;
+}
+superbowlWin(record);
